c7371cfb6d1c8ed9ded5123ef8f58ca3b79bc550
Lakhae <gorkhali.Favorite@gmail.com>
2014-11-13 22:42:49 -0600

    TEST

---
+++ GorkhalisFavorite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GorkhalisFavorite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Little info here";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Gorkhalis";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Gorkahlis contact";
+
+            return View();
+        }
+    }
+}
---
+++ GorkhalisFavorite/Models/GorkhalisFavoriteDb.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Data.Entity;
+
+namespace GorkhalisFavorite.Models
+{
+    public class GorkhalisFavoriteDb : DbContext
+    {
+        public GorkhalisFavoriteDb() : base("name=DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
